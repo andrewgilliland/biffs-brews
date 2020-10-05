602892aa77a51ff680600599bc1cd1d6ff0cff70
Andrew Gilliland <andrewpgilliland@gmail.com>
2020-10-05 14:23:33 -0500

    responsive design

--- gatsby/src/components/Pagination.js
+++ gatsby/src/components/Pagination.js
@@ -28,6 +28,14 @@ const PaginationStyles = styled.div`
       color: var(--grey);
     }
   }
+
+  @media (max-width: 800px) {
+    .word {
+      display: none;
+    }
+
+    font-size: 1.4rem;
+  }
 `;
 
 export default function Pagination({
@@ -46,20 +54,28 @@ export default function Pagination({
 
   return (
     <PaginationStyles>
-      <Link disabled={!hasPrevPage} to={`${base}/${prevPage}`}>
-        &#8592; Prev
+      <Link
+        title="Prev Page"
+        disabled={!hasPrevPage}
+        to={`${base}/${prevPage}`}
+      >
+        &#8592; <span className="word"> Prev</span>
       </Link>
       {Array.from({ length: totalPages }).map((_, i) => (
         <Link
-          key={i}
+          key={`page ${i}`}
           className={currentPage === 1 && i === 0 ? "current" : ""}
           to={`${base}/${i > 0 ? i + 1 : ""}`}
         >
           {i + 1}
         </Link>
       ))}
-      <Link disabled={!hasNextPage} to={`${base}/${nextPage}`}>
-        Next &#8594;
+      <Link
+        title="Next Page"
+        disabled={!hasNextPage}
+        to={`${base}/${nextPage}`}
+      >
+        <span className="word">Next</span> &#8594;
       </Link>
     </PaginationStyles>
   );
