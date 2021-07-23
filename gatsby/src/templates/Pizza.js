fc728bf7d8e3b8549bc867c2b3260a33fea7186a
Andrew Gilliland <andrewpgilliland@gmail.com>
2021-07-23 16:53:22 -0500

    updates singlepizzapage styles

--- gatsby/src/templates/Pizza.js
+++ gatsby/src/templates/Pizza.js
@@ -1,30 +1,37 @@
-import React from "react";
-import { graphql } from "gatsby";
-import Img from "gatsby-image";
-import styled from "styled-components";
-import SEO from "../components/SEO";
+import React from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
+import SEO from '../components/SEO';
+import Container from '../components/Container';
 
-const PizzaGrid = styled.div`
+const BrewStyles = styled.div`
   display: grid;
-  grid-gap: 2rem;
   grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+  grid-gap: 3rem;
+
+  ul {
+    color: var(--pale-yellow);
+  }
 `;
 
 export default function SinglePizzaPage({ data: { pizza } }) {
   return (
     <>
       <SEO title={pizza.name} image={pizza.image?.asset?.fluid?.src} />
-      <PizzaGrid>
-        <Img fluid={pizza.image.asset.fluid} />
-        <div>
-          <h2 className="mark">{pizza.name}</h2>
-          <ul>
-            {pizza.toppings.map((topping) => (
-              <li key={topping.id}> {topping.name}</li>
-            ))}
-          </ul>
-        </div>
-      </PizzaGrid>
+      <Container>
+        <BrewStyles>
+          <Img fluid={pizza.image.asset.fluid} />
+          <div>
+            <h2 className="mark">{pizza.name}</h2>
+            <ul>
+              {pizza.toppings.map((topping) => (
+                <li key={topping.id}> {topping.name}</li>
+              ))}
+            </ul>
+          </div>
+        </BrewStyles>
+      </Container>
     </>
   );
 }
